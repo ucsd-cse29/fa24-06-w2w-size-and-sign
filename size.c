a1ad4a2704bdff4c57562d0e3718bd6171265e15
Joe Politz <joe.politz@gmail.com>
2024-10-09 16:07:12 +0000

    sizeof on expressions

--- size.c
+++ size.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 
 int main() {   
-
     printf("%ld\n", sizeof(char));
 
     printf("%ld\n", sizeof(int8_t));
@@ -14,19 +13,20 @@ int main() {
     printf("%ld\n", sizeof(int32_t));
     printf("%ld\n", sizeof(uint32_t));
 
+    printf("%ld\n", sizeof(int64_t));
+    printf("%ld\n", sizeof(int64_t));
+
     printf("%ld\n", sizeof(int));
 
     char c = 128;
     int32_t i = 32;
+    char c2 = c + 1;
 
     printf("sizeof(c): %ld\n", sizeof(c));
     printf("sizeof(i): %ld\n", sizeof(i));
+    printf("sizeof(c * 4000): %ld\n", sizeof(c * 4000));
+    printf("sizeof(c + 1): %ld\n", sizeof(c + 1));
+    printf("sizeof(c2): %ld\n", sizeof(c2));
 
-    int32_t max32signed = 2147483647;
-    printf("%d + 1 = %d\n", max32signed, max32signed + 1);
-    printf("%d + 10000 = %d\n", max32signed, max32signed + 10000);
 
-    uint32_t max32usigned = 2147483647;
-    printf("%d + 1 = %d\n", max32usigned, max32usigned + 1);
-    printf("%d + 10000 = %d\n", max32usigned, max32usigned + 10000);
 }
